c3851403d34652c8b2487913d1e8f770c955890c
mahmoud alhajj <mahmoud.alhajj@et3arraf.com>
2026-06-03 12:20:22 +0300

    Created OnboardingStore with observe.box values for loading, selected ID, and search text.

---
+++ src/stores/OnboardingStore.tsx
@@ -0,0 +1,49 @@
+import {action, makeObservable, observable,runInAction} from 'mobx';
+
+export class OnboardingStore{
+
+    loading = observable.box(false);
+    selectedId = observable.box<number | null>(null);
+    searchText= observable.box('');
+    itemsById=observable.map<number, string>();
+
+
+    set setLoading(loading: boolean) {
+        this.loading.set(loading);
+    }
+    set setSelectedId(id: number){
+        this.selectedId.set(id);
+    }
+    set setSearchText(text: string){
+        this.searchText.set(text);
+    }
+
+    get isLoading() {
+        return this.loading.get();
+    }
+    get getSelectedId() {
+        return this.selectedId.get();
+    }
+    get getSearchText() {
+        return this.searchText.get();
+    }
+
+    async updateItems() {
+        this.setLoading = true;
+
+         const data =
+            await Promise.resolve([
+                { id: 1, name: "Item1" },
+                { id: 2, name: "Item2" }
+            ]);
+
+        runInAction(() => {
+            data.forEach(item => { 
+                this.itemsById.set(item.id, item.name) });
+
+            this.setLoading = false;
+        });
+
+    }
+}
+
