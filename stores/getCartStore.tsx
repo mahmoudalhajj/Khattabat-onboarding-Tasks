c5b9c42cef7820379fdedad9ac09b27ffe760cde
mahmoud alhajj <mahmoud.alhajj@et3arraf.com>
2026-06-04 12:08:16 +0300

    Add getCartStore function to memoize CartStore instances

---
+++ stores/getCartStore.tsx
@@ -0,0 +1,8 @@
+import memoize from "lodash/memoize";
+import { CartStore } from "./CartStore";
+ import { cartItem } from '../types/cartItem';
+
+export const getCartStore = memoize(
+    (item: cartItem) => { return new CartStore(); },
+    (item) => item.id
+);
